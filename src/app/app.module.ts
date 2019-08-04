c867b6d4a1eeec28272e8eff4ba64d18bd811483
v_n_z list.ru <v_n_z@list.ru>
2019-08-04 09:58:44 +0300

    Add nav site bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SitebarComponent } from './sitebar/sitebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SitebarComponent
   ],
   imports: [
     BrowserModule,
